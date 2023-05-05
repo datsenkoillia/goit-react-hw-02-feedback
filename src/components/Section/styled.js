6c3240b0f8832edfdc07fc9e62495170acb5969d
Illia Datsenko <datsenko.i@gmail.com>
2023-05-05 14:54:08 +0300

    Update styled.js

--- src/components/Section/styled.js
+++ src/components/Section/styled.js
@@ -1,31 +1,6 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-color: green;
-margin-bottom: 20px;
+  color: green;
+  margin-bottom: 20px;
 `;
-
-// export const Image = styled.img`
-//   height: 50px;
-// `;
-
-// export const ButtonList = styled.ul`
-//   display: flex;
-//   gap: 15px;
-//   justify-content: left;
-
-// `;
-
-// export const ButtonItem = styled.li`
-//   list-style: none;
-// `;
-
-// export const Button = styled.button`
-//   font-weight: 600;
-
-//   height: 24px;
-//   border-radius: 4px;
-//   padding: 0 15px;
-//   transition: 300ms;
-  
-// `;
